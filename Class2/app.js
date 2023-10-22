79e024218717d645b0968ffdccc5c6e505a3517a
Ahsan Sajjad <princeahsan0608@gmail.com>
2023-10-23 00:08:01 +0500

    Ternary op - fat Arrow fun - Spread operator

---
+++ Class2/app.js
@@ -0,0 +1,52 @@
+// const arr = ["ahsan","ali","usama"]
+
+// arr[0]="abdul"
+
+// console.log(arr)
+
+// Output ['abdul', 'ali', 'usama']
+
+//-------------------------------------------------
+
+// ... in function it's a rest operator
+
+// ... in Array , Array of obj and obj it's a spred op
+
+// var arr1 = [1,2,3]
+// var arr2 = [9,6,3]
+// var arr3=[...arr1,...arr2]
+// console.log(arr3)
+
+// Output [1, 2, 3, 9, 6, 3]
+
+
+//--------------------------
+
+// var obj={
+    //     fname:"ahsan",
+    //     lname:"Sajjad"
+    // }
+// var obj1={
+//     ...obj,
+//     age:20
+// }
+// console.log(obj1)
+
+// Output {fname: 'ahsan', lname: 'Sajjad', age: 20}
+
+
+//-------------------------------------------------
+
+//Fat Arrow function
+
+// let abc = ()=>{
+//     console.log("hello")
+// }
+// abc()
+
+//-------------------------------------------------
+
+//Ternary Operator
+
+// var a = (5==5? "yes":"No")
+// console.log(a)
